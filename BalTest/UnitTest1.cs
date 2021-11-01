49394813a29fc9224e4796e5a1039fd4cda2ba9e
beadarsh <beadarsh@gmail.com>
2021-11-01 06:23:02 +0000

    Initial Project commit. Has AngularJS, BootStrap and JQuery

---
+++ BalTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BalTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
